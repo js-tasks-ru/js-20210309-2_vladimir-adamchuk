9652cafb6db8804397f427be7fd00afb1dd2af2b
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-03-23 23:37:08 +0300

    4 Задачи

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,14 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  const prepare = string.slice(0, size);
+  const tail = [...string.slice(size)];
+  return tail.reduce((spinString, char) => {
+    if (!spinString.endsWith(char.repeat(size))) {
+      spinString += char;
+    }
+    return spinString;
+  }, prepare)
+  if (size == 0) return '';
+  if (size == undefined) return string;
 }
